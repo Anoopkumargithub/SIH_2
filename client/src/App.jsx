6ffa5677e46ecca1bafe2d00a86648a9931b2d3f
Anoopkumargithub <anoopkumar2522004@gmail.com>
2024-08-21 19:10:39 +0100

    createAcoount &  setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateAccount from './components/CreateAccount';
+import './index.css';
+
+const App = () => {
+  return (
+    
+      <CreateAccount />
+
+  );
+};
+
+export default App;
