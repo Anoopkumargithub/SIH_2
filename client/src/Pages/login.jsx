5275298f0013786db2e9aad9faa627fddc6fa08e
prachi <prachiagrawal509@gmail.com>
2024-08-27 12:29:21 +0530

    chnages in login page

--- client/src/Pages/login.jsx
+++ client/src/Pages/login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar';
 
 const Login = () => {
   const [email, setEmail] = useState('');
