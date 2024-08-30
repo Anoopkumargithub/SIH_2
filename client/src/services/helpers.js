ba3f1831a9c72e009f78f8600a10933ff14e9c98
Akarsh Rai <akarshrai1405@gmail.com>
2024-08-31 00:32:15 +0530

    helper dev fix

--- client/src/services/helpers.js
+++ client/src/services/helpers.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-
+const dev = false;
 export const axios = Axios.create({
-    baseURL: "https://nexcarrier-i4r1.onrender.com",
+    baseURL: (dev ? "http://localhost:8000": "https://nexcarrier-i4r1.onrender.com")
 });
