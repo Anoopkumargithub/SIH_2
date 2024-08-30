8ef68424f6034e7dfc0474055d6f65056f3e11c4
Akarsh Rai <akarshrai1405@gmail.com>
2024-08-30 16:21:55 +0530

    deployement321

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "default-src 'self'; connect-src 'self' https://sih-2-12.onrender.com");
+    next();
+  });
+
+  app.use(cors({
+    origin: 'https://nexcarrier.onrender.com', // Ensure this matches your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
 export default app; 
