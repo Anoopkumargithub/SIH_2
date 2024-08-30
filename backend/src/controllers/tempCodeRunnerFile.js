87347b4fd0690ada93a14938811bf1a6ee605747
Akarsh Rai <akarshrai1405@gmail.com>
2024-08-30 12:53:54 +0530

    deployement1

--- backend/src/controllers/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
- try {
-        const user = await User.findById(req.user._id).select('name', 'email');
-        const profile = await Profile.findOne({ user: req.user._id });
-
-        if (!user || !profile) {
-            return res.status(404).json({ error: 'User or profile not found' });
-        }
