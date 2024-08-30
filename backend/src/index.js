e43d8a078f2d6e829ac012e8b566942f5fbb6437
Akarsh Rai <akarshrai1405@gmail.com>
2024-08-30 13:50:14 +0530

    deployement31

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   });
   
 app.use(cors({
-    origin: 'https://nexcarrier.onrender.com/', // Ensure this matches your frontend URL
+    origin: 'https://nexcarrier.onrender.com', // Ensure this matches your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
